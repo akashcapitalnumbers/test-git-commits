f83cb3df6728260370a9b9895ed61e8b3d81bafd
akashcapitalnumbers <akash@capitalnumbers.com>
2024-07-02 13:38:39 +0530

    this is just a test project for configuring git

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.tsx"
+import "./index.css"
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+
+/* this is not it 
+
+this is supposed to be purple but it not looking purple to my eye*/
+
+/* this are not that bad if i ty to  */
